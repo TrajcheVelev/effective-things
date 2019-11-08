374e7906d7288c700222f4029a83e40c27e09d36
Trajche Velev <velevtaki@gmail.com>
2019-11-08 10:21:33 +0100

    Add

---
+++ my-app/src/SlideShow.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+class SlideShow extends Component{
+    render(){
+      return(
+        <slider>
+        <slide></slide>
+        <slide></slide>
+        <slide></slide>
+        <slide></slide>
+       </slider>
+
+
+
+      )
+    }
+  }
+export default SlideShow;
